e9aa70b8d486f4bd07b08f9c763c557afd016f64
Alex <alemskx@gmail.com>
2024-10-24 21:33:13 +0300

    задание 2

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 str1 = str(input("Введите первую строку: ")).lower()
 str2 = str(input("Введите вторую строку: ")).lower()
-if str1 == str2:
-    print("Строки не являются анаграммами.")
-    
 if len(str1)!=len(str2):
     print("Строки не являются анаграммами.")
-
-if sorted(str1) == sorted(str2):
+elif sorted(str1) == sorted(str2):
     print("Строки являются анаграммами.")
 else:
     print("Строки не являются анаграммами.")
